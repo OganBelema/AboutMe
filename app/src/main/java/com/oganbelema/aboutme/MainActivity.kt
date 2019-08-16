28fde72ed35411c563e3b334f024ec6c23236ce3
tamunobelemamiebaka-ogan <belema@softcom.ng>
2019-08-16 09:00:22 +0100

    Add click listener for button and hide keyboard

--- app/src/main/java/com/oganbelema/aboutme/MainActivity.kt
+++ app/src/main/java/com/oganbelema/aboutme/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.oganbelema.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        done_button.setOnClickListener {
+            nickname_text.text = nickname_edit.text
+            nickname_edit.visibility = View.GONE
+            done_button.visibility = View.GONE
+            nickname_edit.visibility = View.VISIBLE
+
+            //Hide keyboard
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(done_button.windowToken, 0)
+        }
+
     }
+
+
 }
